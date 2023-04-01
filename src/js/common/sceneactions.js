60677b1b50b3400ec78682100d7306d019431f65
Lennart Demes <31403260+struffel@users.noreply.github.com>
2023-04-01 13:41:04 +0200

    Add loading indicator

--- src/js/common/sceneactions.js
+++ src/js/common/sceneactions.js
@@ -1,26 +1,34 @@
 import * as THREE from "../threejs/three.module.js";
 import * as RGBE_LOADER from '../threejs/RGBELoader.js';
 import * as EXR_LOADER from '../threejs/EXRLoader.js';
+import * as BASE from "../common/base.js";
+import * as LOADING from "../common/loading.js";
 
 /**
  * Updates the environment of a given ThreeJS-scene.
+ * This happens in multiple places and is therefore defined as its own dedicated function.
  */
 export function updateSceneEnvironment(url,scene,renderer){
 
-	// Decide which loader to use
-	if(url.split("?")[0].split("#")[0].endsWith(".hdr")){
-		var envLoader = new THREE.RGBELoader();
-	}else if(url.split("?")[0].split("#")[0].endsWith(".exr")){
-		var envLoader = new THREE.EXRLoader();
+	var envFileUrl = url;
+	var envFileName = BASE.filenameFromUrl(url);
+
+	if(envFileUrl.split("?")[0].split("#")[0].endsWith(".hdr")){
+		var envLoader = new RGBE_LOADER.RGBELoader();
+	}else if(envFileUrl.split("?")[0].split("#")[0].endsWith(".exr")){
+		var envLoader = new EXR_LOADER.EXRLoader();
 	}
 
-	// Load the environment
-	envLoader.load(url, texture => {
+	var loadingNote = new LOADING.LoadingNote(envFileName,envFileUrl);
+	loadingNote.start();
+	envLoader.load(envFileUrl, texture => {
 		const gen = new THREE.PMREMGenerator(renderer);
 		const envMap = gen.fromEquirectangular(texture).texture;
 		scene.environment = envMap;
 		scene.background = envMap;
-		texture.dispose();
-		gen.dispose();
+		texture.dispose()
+		gen.dispose()
+
+		loadingNote.finish();
 	});
 }
